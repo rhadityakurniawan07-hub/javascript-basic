d7fd0c9508161343e071e872056003564cee65da
unknown <rhadityakurniawan07-hub>
2026-07-28 10:51:58 +0700

    menyelesaikan latihan 1

---
+++ bab-03-variable/latihan/latihan01/index.js
@@ -0,0 +1,11 @@
+console.log("=====Biodata=====");
+const namaLengkap = "Rhadit Kurniawan"
+let umur = 19;
+let kota = "jakarta";
+let pekerjaan = "pelajar";
+
+
+console.log("nama :", namaLengkap)
+console.log("umur :", umur)
+console.log("kota :", kota)
+console.log("pekerjaan :", pekerjaan)
