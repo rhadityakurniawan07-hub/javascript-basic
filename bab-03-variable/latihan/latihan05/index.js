6d26d271446e2afbd6f04ece362c5da180b13ad7
unknown <rhadityakurniawan07-hub>
2026-07-28 14:36:02 +0700

    membuat js latihan 05

---
+++ bab-03-variable/latihan/latihan05/index.js
@@ -0,0 +1,10 @@
+const nilaiMatematika = 78;
+const nilaiIpa = 25;
+const nilaiBahasaIndonesia = 34;
+
+const rataRata = (nilaiMatematika + nilaiIpa + nilaiBahasaIndonesia) / 3;
+
+console.log("Nilai Matematika :", nilaiMatematika);
+console.log("Nilai IPA :", nilaiIpa);
+console.log("Nilai Bahasa Indonesia :", nilaiBahasaIndonesia);
+console.log("Rata-rata Nilai :", rataRata);
