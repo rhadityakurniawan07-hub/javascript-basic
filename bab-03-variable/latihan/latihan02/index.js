df50446311cc2fda67cc51ebfe6897f554d17eef
unknown <rhadityakurniawan07-hub>
2026-07-28 13:55:22 +0700

    membuat js latihan 01

---
+++ bab-03-variable/latihan/latihan02/index.js
@@ -0,0 +1,5 @@
+let saldo = 100000;
+
+console.log("Saldo Awal :", saldo);
+saldo += 50000;
+console.log("Saldo Akhir :", saldo);
---
+++ bab-03-variable/latihan/latihan03/index.js
@@ -0,0 +1,10 @@
+const namaProduk = "Al-qur'an";
+const hargaSatuan = 30000;
+
+let jumlahDibeli = 4;
+let totalAkhir = hargaSatuan * jumlahDibeli
+
+console.log("Produk    :", namaProduk);
+console.log("Harga     : Rp", hargaSatuan);
+console.log("Jumlah    :", jumlahDibeli);
+console.log("Total     :", totalAkhir);
