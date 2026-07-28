f3146c1600153f8afc83e2087662243f48b1af87
unknown <rhadityakurniawan07-hub>
2026-07-28 14:10:58 +0700

    membuat js latihan 04

---
+++ bab-03-variable/latihan/latihan04/index.js
@@ -0,0 +1,9 @@
+const nama = "Rhaditya Kurniawan";
+const tahunLahir = 2007;
+const tahunSekarang = 2026;
+const umur = tahunSekarang - tahunLahir;
+
+console.log("Nama           :", nama);
+console.log("Tahun Lahir    :", tahunLahir);
+console.log("Tahun Sekarang :", tahunSekarang);
+console.log("Umur           :", umur, "tahun");
