845352ea9bd209873662dca166e401c78332e26f
unknown <rhadityakurniawan07-hub>
2026-08-08 05:48:37 +0700

    menyelesaikan bab 11

--- bab-11-iterable-dan-non-iterable/latihan/soal1.js
+++ bab-11-iterable-dan-non-iterable/latihan/soal1.js
@@ -1,1 +1,8 @@
 // Iterable atau Non-Iterable
+/*
+1. "Indonesia" → Iterable, karena String dapat diulang setiap karakternya.
+2. ["Merah", "Putih"] → Iterable, karena Array dapat diulang setiap elemennya.
+3. 150 → Non-Iterable, karena Number tidak dapat diulang.
+4. true → Non-Iterable, karena Boolean tidak dapat diulang.
+5. { nama: "Budi" } → Non-Iterable, karena Object biasa tidak dapat langsung diulang dengan for...of.
+*/
